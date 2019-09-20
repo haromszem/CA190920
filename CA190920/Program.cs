951cfc3a2f9bfa226c363148b8c728783f770681
perjesi.daniel@vereb.dc <perjesi.daniel@vereb.dc>
2019-09-20 12:00:41 +0200

    Add project files.

---
+++ CA190920/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA190920
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Szia, hogy hívnak?");
+            var nev= Console.ReadLine();
+            Console.WriteLine("Milyen szép név az hogy {0} UwU",nev);
+            Console.WriteLine("Szeretsz programozni?");
+            var valasz = Console.ReadLine();
+            if (valasz == "igen")
+            {
+                Console.WriteLine("Akkor jó barátok leszünk!");
+
+            }
+            else
+            {
+                Console.WriteLine("-szomorú smile-");
+                Console.WriteLine("Viszlát...");
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
